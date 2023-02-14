2936141fa1420300851770f0272938b051f116e1
bburt8701 <bburt8701@gmail.com>
2023-02-13 18:43:30 -0600

    Changed to point towards glycine mine

--- src/data-sources/index.js
+++ src/data-sources/index.js
@@ -2,7 +2,7 @@ const { IntermineAPI } = require('./intermine/intermine.api.js');
 
 const dataSources = () => {
   return {
-    lisIntermineAPI: new IntermineAPI('https://mines.legumeinfo.org/minimine/service'),
+    lisIntermineAPI: new IntermineAPI('https://mines.legumeinfo.org/glycinemine/service'),
   };
 };
 
